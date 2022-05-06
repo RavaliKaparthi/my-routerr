ddaf5902daee0e51fb041fbb4e122098a6f681bb
ravalikparathi <ravali2001@gmail.com>
2022-05-06 09:24:30 +0530

    page not found

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { About } from './components/About';
 import React from 'react'
 import { Navbar } from './components/Navbar';
 import { OrderSummary } from './components/OrderSummay';
+import { NoMatch } from './components/NoMatch';
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/order-summary" element={<OrderSummary />}></Route>
+        <Route path="*" element={<NoMatch/>}></Route>
+
     </Routes>
     </div>
   );
